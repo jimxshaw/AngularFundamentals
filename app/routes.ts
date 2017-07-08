af52fa4e41a4ab92a8407c0a5e91dcb0871c3990
Jim Shaw <jimxshaw@gmail.com>
2017-07-08 10:44:30 -0500

    Created resolver service, used it with events route in routes.ts. Called the resolver in EventsListComponent.

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,8 @@ import {EventDetailsComponent} from "./events/event-detail/event-details.compone
 import {CreateEventComponent} from "./events/create-event.component";
 import {Status404Component} from "./errors/status-404.component";
 import {Routes} from "@angular/router";
-import {EventRouteActivatorService} from "./events/event-route-activator.service"; // TS type definition for extra intellisense and compile time safety in our IDE.
+import {EventRouteActivatorService} from "./events/event-route-activator.service";
+import {EventsListResolverService} from "./events/events-list-resolver.service"; // TS type definition for extra intellisense and compile time safety in our IDE.
 
 // Whenever the URL hits /events/ then show the EventsListComponent
 // wherever the <router-outlet> component is placed. That currently
@@ -14,8 +15,11 @@ import {EventRouteActivatorService} from "./events/event-route-activator.service
 // The canActivate/canDeactivate properties take in an array of route guard services or functions that
 // specify rules of if/when a particular route can be accessed or prevent navigating away from a route.
 export const appRoutes: Routes = [
-    {path: "events/new", component: CreateEventComponent, canDeactivate: ["canDeactivateCreateEvent"]}, // This uses a function rather a service but still needs to be registered in AppModule's providers.
-    {path: "events", component: EventsListComponent},
+    // This uses a function rather a service but still needs to be registered in AppModule's providers.
+    {path: "events/new", component: CreateEventComponent, canDeactivate: ["canDeactivateCreateEvent"]},
+    // Before resolving this route, call the resolver service. When the resolver finishes and returns
+    // some data, add that data to the route as a property named events.
+    {path: "events", component: EventsListComponent, resolve: {events: EventsListResolverService}},
     {path: "events/:id", component: EventDetailsComponent, canActivate: [EventRouteActivatorService]},
     {path: "404", component: Status404Component},
     {path: "", redirectTo: "/events", pathMatch: "full"}, // Default route.
