9eaa75922e59f4167d823080729ae595c847da5b
Jim Shaw <jimxshaw@gmail.com>
2017-07-28 09:43:46 -0500

    Inject AuthService into NavbarComponent. Use AuthService in NavbarComponent template to change the heading depending whether or not a user is logged in.

--- app/nav/navbar.component.ts
+++ app/nav/navbar.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {AuthService} from "../user/auth.service";
 
 @Component({
     selector: "nav-bar",
@@ -25,4 +26,15 @@ import {Component} from "@angular/core";
 })
 export class NavbarComponent {
 
+    constructor(private authService: AuthService) {
+
+    }
+
 }
+
+
+
+
+
+
+
