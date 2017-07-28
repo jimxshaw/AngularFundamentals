4988894ef249674fb281799eb5886be0e485be6c
Jim Shaw <jimxshaw@gmail.com>
2017-07-28 09:58:08 -0500

    Hook up submit and cancel clisks in LoginComponent template.

--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -1,12 +1,13 @@
 import {Component} from "@angular/core";
 import {AuthService} from "./auth.service";
+import {Router} from "@angular/router";
 
 @Component({
     templateUrl: "/app/user/login.component.html"
 })
 export class LoginComponent {
 
-    constructor(private authService: AuthService) {
+    constructor(private authService: AuthService, private router: Router) {
 
     }
 
@@ -15,5 +16,11 @@ export class LoginComponent {
         // All the field values from that template form is already encompassed in the passed in
         // argument. Thus, we can access those field values using the usual dot notation.
         this.authService.loginUser(formValues.userName, formValues.password);
+        this.router.navigate(["events"]);
+
+    }
+
+    cancel() {
+        this.router.navigate(["events"]);
     }
 }
