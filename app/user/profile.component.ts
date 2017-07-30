229b5e7de8baf59ceb7c543a068dcfa183703031
Jim Shaw <jimxshaw@gmail.com>
2017-07-29 19:55:03 -0500

    Add further validation styling to ProfileComponent.

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -6,27 +6,47 @@ import {AuthService} from "./auth.service";
 @Component({
     templateUrl: "app/user/profile.component.html",
     styles: [`
-        em { float: right; color: #E05C65; padding-left: 10px; }
-        .error input { background-color: #E3C3C5; }
-        .error ::-webkit-input-placeholder { color: #999 }
+        em {
+            float: right;
+            color: #E05C65;
+            padding-left: 10px;
+        }
+
+        .error input {
+            background-color: #E3C3C5;
+        }
+
+        .error ::-webkit-input-placeholder {
+            color: #999
+        }
     `]
 })
 export class ProfileComponent implements OnInit {
 
-    profileForm: FormGroup;
+    public profileForm: FormGroup;
+    private firstName: FormControl;
+    private lastName: FormControl;
 
     constructor(private router: Router, private authService: AuthService) {
 
     }
 
     ngOnInit(): void {
-        let firstName = new FormControl(this.authService.currentUser.firstName, Validators.required);
-        let lastName = new FormControl(this.authService.currentUser.lastName, Validators.required);
+        this.firstName = new FormControl(this.authService.currentUser.firstName, Validators.required);
+        this.lastName = new FormControl(this.authService.currentUser.lastName, Validators.required);
 
         this.profileForm = new FormGroup({
-            firstName: firstName,
-            lastName: lastName
+            firstName: this.firstName,
+            lastName: this.lastName
         });
+    }
+
+    validateFirstName() {
+        return this.firstName.valid || this.firstName.untouched;
+    }
+
+    validateLastName() {
+        return this.lastName.valid || this.lastName.untouched;
     }
 
     saveProfile(formValues): void {
