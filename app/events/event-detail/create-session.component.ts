9d2363f655f2b75dd77a8201fb33574f3d0c2d3f
Jim Shaw <jimxshaw@gmail.com>
2017-07-30 17:54:54 -0500

    Add validation to CreateSessionComponent template.

--- app/events/event-detail/create-session.component.ts
+++ app/events/event-detail/create-session.component.ts
@@ -1,8 +1,27 @@
 import {Component, OnInit} from "@angular/core";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ISession} from "../shared/event.model";
 
 @Component({
-    templateUrl: "app/events/event-detail/create-session.component.html"
+    templateUrl: "app/events/event-detail/create-session.component.html",
+    styles: [`
+        em {
+            float: right;
+            color: #E05C65;
+            padding-left: 10px;
+        }
+
+        .error input,
+        .error select,
+        .error textarea {
+            background-color: #E3C3C5;
+        }
+
+        .error ::-webkit-input-placeholder {
+            color: #999
+        }
+    `]
+
 })
 export class CreateSessionComponent implements OnInit {
 
@@ -30,7 +49,18 @@ export class CreateSessionComponent implements OnInit {
     }
 
     saveSession(formValues): void {
-        console.log(formValues);
+        let session: ISession = {
+            id: undefined,
+            name: formValues.name,
+            // Duration is a number and must be cast as such.
+            duration: parseFloat(formValues.duration),
+            level: formValues.level,
+            presenter: formValues.presenter,
+            abstract: formValues.abstract,
+            voters: []
+        };
+
+        console.log(session);
     }
 
 }
