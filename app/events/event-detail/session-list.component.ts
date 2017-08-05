4c292afc128cd7b71ecebab5b8917bfe728737cb
Jim Shaw <jimxshaw@gmail.com>
2017-08-05 16:24:54 -0500

    Add the ability to sort by name and votes to SessionListComponent.

--- app/events/event-detail/session-list.component.ts
+++ app/events/event-detail/session-list.component.ts
@@ -10,11 +10,13 @@ export class SessionListComponent implements OnChanges {
     @Input() sessions: ISession[];
     @Input() filterBy: string;
     visibleSessions: ISession[] = [];
+    @Input() sortBy: string;
 
     ngOnChanges() {
         // Is sessions set? If yes then filter the sessions.
         if (this.sessions) {
             this.filterSessions(this.filterBy);
+            this.sortBy === "name" ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc);
         }
     }
 
@@ -29,4 +31,23 @@ export class SessionListComponent implements OnChanges {
         }
     }
 
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession): number {
+    if (s1.name > s2.name) {
+        return 1;
+    }
+    else if (s1.name === s2.name) {
+        return 0;
+    }
+    else {
+        return -1;
+    }
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession): number {
+    // A positive number, zero or negative number is returned
+    // depending on the comparison between session 2 and session 1's
+    // voter array lengths.
+    return s2.voters.length - s1.voters.length;
 }
