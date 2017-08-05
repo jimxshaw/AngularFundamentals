11cf4598c3d7d5181dd90c471f8e9f255ef46ea9
Jim Shaw <jimxshaw@gmail.com>
2017-08-05 13:50:30 -0500

    Add filterBy property to EventDetailComponent. Add filter by buttons to template.

--- app/events/event-detail/event-details.component.ts
+++ app/events/event-detail/event-details.component.ts
@@ -26,6 +26,7 @@ export class EventDetailsComponent implements OnInit {
 
     event: IEvent;
     addMode: boolean;
+    filterBy: string = "all";
 
     constructor(private eventService: EventService, private activatedRoute: ActivatedRoute) {
 
